d136660cef63663db712e90f1e470bb717ad2b2f
ROHIN <rohit.n@oracle.com>
2024-02-07 01:39:25 +0530

    Initial commit

---
+++ src/main/java/NewJavaProject.java
@@ -0,0 +1,11 @@
+class NewJavaProject{
+    NewJavaProject()
+    {
+        System.out.println("I am constructor");
+    }
+    public static void main(String... s)
+    {
+        NewJavaProject newJavaProject=new NewJavaProject();
+        System.out.println("Hello World");
+    }
+}
